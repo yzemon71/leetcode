4fa42b612854d86a1fff49223e98b39b2bdf8f9b
yzemon71 <yzemon71@users.noreply.github.com>
2022-05-04 04:22:48 +0900

    add problems 8

--- problems/src/main/java/8.string-to-integer-atoi.java
+++ problems/src/main/java/8.string-to-integer-atoi.java
@@ -13,23 +13,24 @@ class Solution {
         Pattern p = Pattern.compile("^[ ]*[-+]?[0-9]+");
         Matcher m = p.matcher(s);
         int rev = 0;
-        if(m.find()){
-            String x = m.group().replace(" ", "");
-            int sign = 1;
-            if(x.charAt(0) == '-'){
-                sign = -1;
-                x = x.substring(1, x.length());
-            } else if(x.charAt(0) == '+'){
-                x = x.substring(1, x.length());
-            }
-    
-            while (x.length() != 0) {
-                int pop = sign * Character.getNumericValue(x.charAt(0));
-                x = x.substring(1, x.length());
-                if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return Integer.MAX_VALUE;
-                if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return Integer.MIN_VALUE;
-                rev = rev * 10 + pop;
-            }
+        if(!m.find()){
+            return 0;
+        }
+        String x = m.group().replace(" ", "");
+        int sign = 1;
+        if(x.charAt(0) == '-'){
+            sign = -1;
+            x = x.substring(1, x.length());
+        } else if(x.charAt(0) == '+'){
+            x = x.substring(1, x.length());
+        }
+
+        while (x.length() != 0) {
+            int pop = sign * Character.getNumericValue(x.charAt(0));
+            x = x.substring(1, x.length());
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return Integer.MAX_VALUE;
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return Integer.MIN_VALUE;
+            rev = rev * 10 + pop;
         }
         return rev;
     }
