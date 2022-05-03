f1725831215707f66952d0277efc280b2a3607e0
yzemon71 <yzemon71@users.noreply.github.com>
2022-05-03 18:01:44 +0900

    add problems 7

--- problems/src/main/java/7.reverse-integer.java
+++ problems/src/main/java/7.reverse-integer.java
@@ -9,6 +9,23 @@ import java.util.List;
  * [7] Reverse Integer
  */
 
+/*
+// another practice
+class Solution {
+    public int reverse(int x) {
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7)) return 0;
+            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8)) return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+}
+ */
+
 // @lc code=start
 class Solution {
     public int reverse(int x) {
